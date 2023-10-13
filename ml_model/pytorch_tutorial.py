c353ffba7388eddc7a0e96fab4a75fb4f142882f
Asfandyar-Khan-2022 <117299102+Asfandyar-Khan-2022@users.noreply.github.com>
2023-10-14 00:26:53 +0100

    prediction added

--- ml_model/pytorch_tutorial.py
+++ ml_model/pytorch_tutorial.py
@@ -1,7 +1,35 @@
-import torch 
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-weights = torch.ones(4, requires_grad=True)
+data = pd.read_csv("colors.csv")
+columns = ["general_color_name", "hex", "Unnamed: 0"]
+data = data.drop(columns, axis=1)
+column_to_move = data.pop("color_name")
+data.insert(3, "color", column_to_move)
+color_list = data["color"].to_list()
+color_list = list(enumerate(color_list))
+color_list_dict = {}
 
-optimizer = torch.optim.SGD(weights, lr=0.01)
-optimizer.step()
-optimizer.zero_grad()
+test_measured = (246, 247, 238)
+like = []
+index = 0
+
+for colors in color_list:
+    color_list_dict[colors[1]] = colors[0]
+    data["color"] = data["color"].replace([colors[1]], [colors[0]])
+    print(colors)
+
+while index < len(data.index):
+    row = data.loc[[index]]
+
+    like.append(
+        round((((min(row.loc[index, "red"], test_measured[0]) / max(row.loc[index, "red"], test_measured[0])) * 100)
+        + ((min(row.loc[index, "green"], test_measured[1]) / max(row.loc[index, "green"], test_measured[1])) * 100) 
+        + ((min(row.loc[index, "blue"], test_measured[2]) / max(row.loc[index, "blue"], test_measured[2])) * 100)) 
+        / 3, 3)
+        )
+    index += 1
+
+list = list(enumerate(like))
+list2 = sorted(list, key=lambda x:-x[1])
+print(list2)
